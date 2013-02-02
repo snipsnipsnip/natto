ee2fd0a5376cd341d4a56c0112c66fd54177dbae
snipsnipsnip <snipsnipsnip@users.sourceforge.jp>
2013-02-03 00:55:53 +0900

    tweak

--- natto.rb
+++ natto.rb
@@ -10,11 +10,19 @@ require 'sinatra/content_for'
 require 'sinatra/static_assets'
 require 'sinatra/config_file'
 require 'sequel'
-require 'sinatra/reloader' if development?
+
+require_relative 'lib/octowalker'
 
 configure do
   config_file 'config.yml'
   set :sessions, {:key => 'z'}
+end
+
+helpers do
+  def github
+    # TODO: per-user auth
+    @github ||= OctoWalker.new(settings.github_auth)
+  end
 end
 
 get '/' do
