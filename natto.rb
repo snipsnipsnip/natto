06a955d52713595126bee4383f85eef3fc2297fe
snipsnipsnip <snipsnipsnip@users.sourceforge.jp>
2013-02-02 18:01:09 +0900

    added config file

--- natto.rb
+++ natto.rb
@@ -3,5 +3,20 @@
 
 # natto: a qiita hackathon 03 product
 
+require 'slim'
+require 'sass'
 require 'sinatra'
+require 'sinatra/content_for'
+require 'sinatra/static_assets'
+require 'sinatra/config_file'
+require 'sequel'
+require 'sinatra/reloader' if development?
 
+configure do
+  config_file 'config.yml'
+  set :sessions, {:key => 'z'}
+end
+
+get '/' do
+  slim :index
+end
