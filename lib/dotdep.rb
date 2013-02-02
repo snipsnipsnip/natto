14cd50cc7ee57ab3bd7602350e335d6487d25a53
snipsnipsnip <snipsnipsnip@users.sourceforge.jp>
2013-02-03 01:57:15 +0900

    replace IO with visitor

--- lib/dotdep.rb
+++ lib/dotdep.rb
@@ -9,8 +9,8 @@ class Dep
   attr_accessor :case_sensitive
   attr_accessor :cluster
 
-  def initialize(sources)
-    @io = STDOUT
+  def initialize(sources, out)
+    @out = out
     @ignore_file_matcher = nil
     @source_code_filters = []
     @case_sensitive = false
@@ -20,12 +20,7 @@ class Dep
 
   def run(globs)
     graph = scan(@source_code_filters, @case_sensitive, list(globs, @ignore_file_matcher))
-    
-    if @cluster && (clusters = calc_cluster(graph)).size >= 2
-      print_cluster(graph, clusters)
-    else
-      print_flat(graph)
-    end
+    print_flat(graph)
   end
   
   private
@@ -59,14 +54,10 @@ class Dep
     tree
   end
   
-  Node = Struct.new(:label, :files, :links, :cluster)
+  Node = Struct.new(:label, :files, :links)
   
   def make_node(label, filename)
-    Node.new(label, [], Set.new, calc_cluster_name(filename))
-  end
-  
-  def calc_cluster_name(filename)
-    File.basename(File.dirname(filename))
+    Node.new(label, [], Set.new)
   end
   
   def calc_label(filepath)
@@ -77,96 +68,27 @@ class Dep
     calc_label(filepath).downcase.gsub('_', '')
   end
   
-  def calc_cluster(dep)
-    dep.group_by {|k,v| v.cluster }
-  end
-  
-  def print_cluster(graph, clusters)
-    links = {}
-    outer_links = Set.new
-    
-    graph.each do |nodename, node|
-      node.links.each do |destname|
-        if clusters[node.cluster].any? {|n, _| n == destname }
-          (links[node.cluster] ||= Set.new) << [nodename, destname]
-        else
-          outer_links << [nodename, destname]
-        end
-      end
-    end
-    
-    print_digraph do
-      indent = '    '
-    
-      links.each_with_index do |(cluster_name, links), i|
-        print_subgraph(i, cluster_name) do
-          clusters[cluster_name].each do |name, node|
-            print_node(graph, name, node, indent)
-          end
-          
-          links.each {|from, to| print_link(from, to, indent) }
-        end
-      end
-      
-      indent = '  '      
-      outer_links.each {|from, to| print_link(from, to, indent) }
-    end
-  end
 
   def print_flat(dep)
-    print_digraph do
-      @io.puts
-      @io.puts '  // nodes'
-      indent = '  '
-      
-      dep.each do |node_name, node|
-        print_node(dep, node_name, node, indent)
-      end
-      
-      @io.puts
-      @io.puts '  // links'
-      
-      dep.each do |s, node|
-        node.links.each do |d|
-          print_link s, d, indent
-        end
+    dep.each do |node_name, node|
+      print_node(dep, node_name, node, indent)
+    end
+    
+    dep.each do |s, node|
+      node.links.each do |d|
+        print_link s, d, indent
       end
     end
-  end
-  
-  def print_subgraph(number, label)
-    @io.puts "  subgraph cluster#{number} {"
-    @io.puts "    label = #{label.inspect};"
-    @io.puts %{   fontcolor = "#123456"; fontsize = 30; fontname="Arial, Helvetica";}
-    
-    yield
-    
-    @io.puts "  }"
   end
   
   def print_link(from, to, indent=nil)
-    @io.puts %{#{indent}"#{from}" -> "#{to}";} if from != to
+    @out.link(from, to) if from != to
   end
   
   def print_node(graph, node_name, node, indent=nil)
-    node.files.each {|f| @io.puts %{#{indent}/* #{f} */} }
     fan_in = graph.count {|n,d| d.links.include?(node_name) }
     fan_out = node.links.size
-    @io.puts %{#{indent}"#{node_name}" [label = "#{node.label}|{#{fan_in} in|#{fan_out} out}", shape = Mrecord];}
-  end
-  
-  def print_digraph
-    @io.puts '// try tred tool of graphviz if you want simpler (transitively reduced) graph'
-    @io.puts 'digraph {'
-    @io.puts '  overlap = false;'
-    @io.puts '  rankdir = LR;'
-    @io.puts '  node [style = filled, fontcolor = "#123456", fillcolor = white, fontsize = 30, fontname="Arial, Helvetica"];'
-    @io.puts '  edge [color = "#661122"];'
-    @io.puts '  bgcolor = "transparent";'
-    
-    yield
-    
-    @io.puts '}'
+    @out.node(node_name, node.label, node.files, fan_in, fan_out)
   end
 end
 
